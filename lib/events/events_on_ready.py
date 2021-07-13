f323a90bf86645eb9f4de650c8c40335d80f2e5b
Ritik Ranjan <ritik0ranjan@gmail.com>
2021-07-13 09:06:13 +0000

    All Set :)

--- lib/events/events_on_ready.py
+++ lib/events/events_on_ready.py
@@ -1,6 +1,5 @@
 import discord, asyncio, aiohttp
 from core import Cog, HelperBot
-from discord import Webhook
 from pytchat import LiveChatAsync
 from datetime import datetime
 
@@ -11,40 +10,44 @@ class OnReady(Cog):
 
     @Cog.listener()
     async def on_ready(self):
+        
         livechat = LiveChatAsync("SpB6TJeRVLc")
+        
         while livechat.is_alive():
             data = await livechat.get()
+            
             async for c in data.async_items():
-                color = discord.Colour.default()
                 footer = "Normal User"
-                if c.author.isChatModerator:
-                    color = discord.Colour.blue()
-                    footer = "Chat Moderator"
-                if c.author.isChatOwner:
-                    color = discord.Colour.orange()
-                    footer = "Chat Owner"
+                
+                if c.author.isChatModerator: footer = "Chat Moderator"
+                if c.author.isChatOwner: footer = "Chat Owner"
                 date_obj = datetime.strptime(c.datetime, "%Y-%m-%d %H:%M:%S")
-                embed = discord.Embed(description=f"```\n{c.message}\n```",
-                                      color=color,
-                                      timestamp=date_obj)
-                embed.set_author(name=f"{c.author.name}", url=c.author.channelUrl)
-                embed.set_thumbnail(url=c.author.imageUrl)
-                embed.set_footer(text=footer)
+                
+                data = {
+                  'username': "Beasty Stats",
+                  'avatar_url': "https://yt3.ggpht.com/ytc/AKedOLSsvX9K4ESvZt94SEKJg1Km6ufjYa_VUhbgXp6h=s176-c-k-c0x00ffffff-no-rj"
+                }
+                data['embeds'] = [
+                  {
+                    'description': f"```\n{c.message}\n```",
+                    'timestamp': date_obj.isoformat(),
+                    'author': {
+                      'name': f"{c.author.name}",
+                      'url': f"{c.author.channelUrl}"
+                    },
+                    'thumbnail': {'url': f"{c.author.imageUrl}"},
+                    'footer': {'text': footer}
+                  }
+                ]
                 
                 for hook in [
                         'https://discord.com/api/webhooks/864089656701485066/4FSi8EfR3WzwY729i2_bm8QF8SVfoEpukcMZAsg_yJcE9H5sHLeU6lZHxlMCBoYAjdpU',
                         'https://discord.com/api/webhooks/864089652410318850/l9A8JqNXqTZrWDkkm5ow9kKGUf3gDc_Sp7POw96tSWFrdV3zDV_6421uZVjTsPXD9XfL'
                 ]:
-                    print(1)
-                    async def send_webhook():
-                        async with aiohttp.ClientSession() as session:
-                            webhook = Webhook.from_url(
-                                hook, session=session)
-                        try:
-                            await webhook.send(embed=embed)
-                        except Exception as e:
-                            print(e)
-
+                  async with aiohttp.ClientSession() as session:
+                      async with session.post(hook, json=data) as response:
+                          if response.status < 300:
+                            break
 
 def setup(bot):
     bot.add_cog(OnReady(bot))
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from core import HelperBot
+from utils.keep_alive import keep_alive
 
 bot = HelperBot()
 
 if __name__ == '__main__':
+    keep_alive()
     bot.run()
