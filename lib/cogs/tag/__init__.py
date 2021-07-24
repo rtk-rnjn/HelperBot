5cf0b93175517c338af7a02ec8839ae05cfc2576
Ritik Ranjan <ritik0ranjan@gmail.com>
2021-07-24 17:15:31 +0000

    :hmm: dont blame this is in testing

--- lib/cogs/tag/__init__.py
+++ lib/cogs/tag/__init__.py
@@ -1,9 +1,9 @@
 from core import HelperBot, Context, Cog
 from discord.ext import commands
-from utils import db
+
 from . import method as mt 
 from discord import Member
-import sqlite3
+from .method import collection
 
 class Tags(Cog):
     """Tag system for SECTOR 17-29"""
@@ -19,84 +19,45 @@ class Tags(Cog):
     async def create_tag(self, ctx: Context, name: commands.clean_content, *,
                          text: commands.clean_content):
         """To create a tag"""
-        async with db.connect('data/db.db') as conn:
-            async with conn.cursor() as cursor:
-                try:
-                    await mt._create_tag(conn, cursor, 'tags', name,
-                                           ctx.author.id, text)
-                    await ctx.send('OK')
-                except Exception as e:
-                    await ctx.send(f'{e}')
-            await conn.close()
+        await mt._create_tag(name, ctx.author.id, text)
 
     @tag.command(name='delete')
     async def delete_tag(self, ctx: Context, name: commands.clean_content):
         """To delete a tag you own"""
-        async with db.connect('data/db.db') as conn:
-            async with conn.cursor() as cursor:
-                try:
-                    await mt._delete_tag(conn, cursor, 'tags', name,
-                                           ctx.author.id)
-                    await ctx.send('OK')
-                except Exception as e:
-                    await ctx.send(f'{e}')
-            await conn.close()
+        data = collection.find_one({'_id': name, 'owner': ctx.author.id})
+        if not data: return
+        await mt._delete_tag(name)
 
     @tag.command(name='editname')
     async def edit_tag_name(self, ctx: Context, name: commands.clean_content,
                             text: commands.clean_content):
         """To edit the name of tag you own"""
-        async with db.connect('data/db.db') as conn:
-            async with conn.cursor() as cursor:
-                try:
-                    await mt._update_tag_name(conn, cursor, 'tags', name,
-                                                ctx.author.id, text)
-                    await ctx.send('OK')
-                except Exception as e:
-                    await ctx.send(f'{e}')
-            await conn.close()
+        data = collection.find_one({'_id': name, 'owner': ctx.author.id})
+        if not data: return
+        await mt._update_tag_name(name, text)
 
     @tag.command(name='edittext')
     async def edit_tag_text(self, ctx: Context, name: commands.clean_content,
                             *, text: commands.clean_content):
         """To edit the content of tag you own"""
-        async with db.connect('data/db.db') as conn:
-            async with conn.cursor() as cursor:
-                try:
-                    await mt._update_tag_text(conn, cursor, 'tags', name,
-                                                ctx.author.id, text)
-                    await ctx.send('OK')
-                except Exception as e:
-                    await ctx.send(f'{e}')
-            await conn.close()
+        data = collection.find_one({'_id': name, 'owner': ctx.author.id})
+        if not data: return
+        await mt._update_tag_text(name, text)
     
     @tag.command(name='transferownership', aliases=['to'])
     async def tag_transfer(self, ctx: Context, name: commands.clean_content, *, member: Member):
         """To tansfer the tag ownership"""
-        async with db.connect('data/db.db') as conn:
-            async with conn.cursor() as cursor:
-                try:
-                    await mt._tranfer_tag_ownership(conn, cursor, 'tags', name, ctx.author.id, member.id)
-                    await ctx.send('OK')
-                except Exception as e:
-                    await ctx.send(f'{e}')
-            await conn.close()
+        data = collection.find_one({'_id': name, 'owner': ctx.author.id})
+        if not data: return
+        await mt._tranfer_tag_ownership(name, member.id)
 
     @tag.command(name='show')
     async def show_tag(self, ctx: Context, name: commands.clean_content):
         """To show the tag"""
+        data = collection.find_one({'_id': name})
+        if not data: return
         await ctx.message.delete()
-        conn = sqlite3.connect('data/db.db')
-        cursor = conn.cursor()
-        try:
-          data = cursor.execute(f'''SELECT * FROM tags WHERE name="{name}"''')
-          for data in data:
-              if not data: return 
-              else:
-                await ctx.send(f"{data[-1]}")
-        except Exception as e:
-            await ctx.send(f'{e}')
-        conn.close()
+        await ctx.send(f"{data['text']}")
   
 def setup(bot):
     bot.add_cog(Tags(bot))
--- lib/cogs/tag/method.py
+++ lib/cogs/tag/method.py
@@ -1,40 +1,32 @@
-async def _create_tag(con, cur, db_name, name, _id, text):
-    data = await cur.execute(
-        '''INSERT INTO {} VALUES(?, ?, ?)'''.format(db_name),
-        (name, _id, text))
-    await con.commit()
-    return data
+from pymongo import MongoClient
+import os
+
+my_secret = os.environ['DB_KEY']
+
+cluster = MongoClient(
+    f"mongodb+srv://user:{str(my_secret)}@cluster0.xjask.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+)
+db = cluster["tags"]
+collection = db["tags"]
 
 
-async def _update_tag_name(con, cur, db_name, name, _id, text):
-    data = await cur.execute(
-        '''UPDATE {} SET name="{}" WHERE id={} name="{}"'''.format(
-            db_name, text, _id, name))
-    await con.commit()
-    return data
+async def _create_tag(_id: str, owner: int, text: str):
+    collection.insert_one({'_id': _id, 'owner': owner, 'text': text})
 
 
-async def _update_tag_text(con, cur, db_name, name, _id, text):
-    data = await cur.execute(
-        '''UPDATE {} SET text="{}" WHERE id={} name="{}"'''.format(
-            db_name, text, _id, name))
-    await con.commit()
-    return data
+async def _update_tag_name(_id: str, new_id: str):
+    data = collection.find_one({'_id': new_id})
+    if not data:
+        collection.update_one({'_id': _id}, {"$set": {'_id': new_id}})
 
 
-async def _delete_tag(con, cur, db_name, name, _id):
-    data = await cur.execute(
-        '''DELETE FROM {} WHERE name="{}" AND id={}'''.format(
-            db_name, name, _id))
-    await con.commit()
-    return data
+async def _update_tag_text(_id: str, text: str):
+    collection.update_one({'_id': _id}, {'$set': {'text': text}})
 
 
-async def _tranfer_tag_ownership(con, cur, db_name, name, _id, member_id):
-    data = await cur.execute(
-      '''UPDATE {} SET id={} WHERE name="{}" AND id={}'''.format(
-        db_name, member_id, name, _id
-      )
-    )
-    await con.commit()
-    return data
+async def _delete_tag(_id: str):
+    collection.delete_one({'_id': _id})
+
+
+async def _tranfer_tag_ownership(_id: str, new_owner: int):
+    collection.update_one({'_id': _id}, {"$set": {'owner': new_owner}})
