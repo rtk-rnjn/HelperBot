544025f908577bb995d0cdff71ee9e93fa54e279
Ritik Ranjan <ritik0ranjan@gmail.com>
2021-07-25 09:38:17 +0000

    updated tag, apply mod, closed

--- lib/cogs/tag/__init__.py
+++ lib/cogs/tag/__init__.py
@@ -20,42 +20,47 @@ class Tags(Cog):
                          text: commands.clean_content):
         """To create a tag"""
         await mt._create_tag(name, ctx.author.id, text)
+        await ctx.send(f"TAG CREATED :white_check_mark: \n\nTag name: **{name}**\nTotal Length of char: **{len(text)}**\nOwner of Tag: **{ctx.author.name}#{ctx.author.discriminator}**")
 
     @tag.command(name='delete')
     async def delete_tag(self, ctx: Context, name: commands.clean_content):
         """To delete a tag you own"""
         data = collection.find_one({'_id': name, 'owner': ctx.author.id})
-        if not data: return
+        if not data: return await ctx.send(f"You are not authorize to do that")
         await mt._delete_tag(name)
+        await ctx.send(f"TAG DELETED :wastebasket::white_check_mark:")
 
     @tag.command(name='editname')
     async def edit_tag_name(self, ctx: Context, name: commands.clean_content,
                             text: commands.clean_content):
         """To edit the name of tag you own"""
         data = collection.find_one({'_id': name, 'owner': ctx.author.id})
-        if not data: return
+        if not data: return await ctx.send(f"You are not authorize to do that")
         await mt._update_tag_name(name, text)
+        await ctx.send(f"TAG UPDATED :white_check_mark: \n\nTag name: **{text}**\nOwner of Tag: **{ctx.author.name}#{ctx.author.discriminator}**")
 
     @tag.command(name='edittext')
     async def edit_tag_text(self, ctx: Context, name: commands.clean_content,
                             *, text: commands.clean_content):
         """To edit the content of tag you own"""
         data = collection.find_one({'_id': name, 'owner': ctx.author.id})
-        if not data: return
+        if not data: return await ctx.send(f"You are not authorize to do that")
         await mt._update_tag_text(name, text)
+        await ctx.send(f"TAG UPDATED :white_check_mark: \n\nTag name: **{name}**\nTotal Length of char: **{len(text)}**\nOwner of Tag: **{ctx.author.name}#{ctx.author.discriminator}**")
     
     @tag.command(name='transferownership', aliases=['to'])
     async def tag_transfer(self, ctx: Context, name: commands.clean_content, *, member: Member):
         """To tansfer the tag ownership"""
         data = collection.find_one({'_id': name, 'owner': ctx.author.id})
-        if not data: return
+        if not data: return await ctx.send(f"You are not authorize to do that")
         await mt._tranfer_tag_ownership(name, member.id)
+        await ctx.send(f"TAG OWNERSHIP TRANSFERED: :white_check_mark:\n\nOwner of Tag: **{member.name}#{member.discriminator}**")
 
     @tag.command(name='show')
     async def show_tag(self, ctx: Context, name: commands.clean_content):
         """To show the tag"""
         data = collection.find_one({'_id': name})
-        if not data: return
+        if not data: return await ctx.send(f"You are not authorize to do that")
         await ctx.message.delete()
         await ctx.send(f"{data['text']}")
   
