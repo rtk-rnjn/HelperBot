c44cc84a80037d6cdcf81b529ea099530bfbdc77
Ritik Ranjan <ritik0ranjan@gmail.com>
2021-08-02 14:53:30 +0000

    all set

--- lib/events/events_on_message.py
+++ lib/events/events_on_message.py
@@ -5,19 +5,21 @@ from core import Cog, HelperBot
 class OnMessageMod(Cog):
     def __init__(self, bot: HelperBot):
         self.bot = bot
-        self.channel = self.bot.get_channel(837637146453868554)
-    
+        self.channel = None
+
     @Cog.listener()
     async def on_message(self, message):
-        if message.guild or message.author.bot: return
-        channel = self.channel
-      
+        await self.bot.wait_until_ready()
+        if message.author.bot: return
+        if self.channel is None:
+            self.channel = self.bot.get_channel(837637146453868554)
+
         if str(message.channel.type) == 'private':
-            await channel.send(
+            await self.channel.send(
                 f"**{message.author.name}#{message.author.discriminator} ({message.author.id})**: {message.content}"
             )
 
-        if message.channel is channel:
+        if message.channel is self.channel:
             if str(message.content).startswith('<'):
                 user = message.mentions[0]
                 await user.send(
