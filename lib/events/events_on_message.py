e91dc6520078675ae3866c7f8865500828b4bc4f
Ritik Ranjan <ritik0ranjan@gmail.com>
2021-08-02 14:20:57 +0000

    forget to push

--- lib/events/events_on_message.py
+++ lib/events/events_on_message.py
@@ -5,23 +5,23 @@ from core import Cog, HelperBot
 class OnMessageMod(Cog):
     def __init__(self, bot: HelperBot):
         self.bot = bot
-
+        self.channel = self.bot.get_channel(837637146453868554)
+    
     @Cog.listener()
     async def on_message(self, message):
-        if not message.guild or message.author.bot: return
-
-        channel = discord.utils.get(message.guild.channels, name='mod-mail')
-
+        if message.guild or message.author.bot: return
+        channel = self.channel
+      
         if str(message.channel.type) == 'private':
             await channel.send(
-                f"**{message.author.name}#{message.author.discriminator} ({message.author.id})** {message.content}"
+                f"**{message.author.name}#{message.author.discriminator} ({message.author.id})**: {message.content}"
             )
 
         if message.channel is channel:
             if str(message.content).startswith('<'):
                 user = message.mentions[0]
                 await user.send(
-                    f"**{message.author.name}#{message.author.discriminator}** {message.content}"
+                    f"**{message.author.name}#{message.author.discriminator}**: {message.content}"
                 )
 
 
