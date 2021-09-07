b2bbd422d6333275ae51371dcbbf827eff2e5605
Ritik Ranjan <37435729+ritik0ranjan@users.noreply.github.com>
2021-09-07 22:38:08 +0530

    ..

--- utils/config.py
+++ utils/config.py
@@ -4,13 +4,11 @@ TOKEN = os.environ['TOKEN']
 EXTENTIONS = [
     'jishaku',
     'lib.cogs.youtube',
-    'lib.cogs.ult',
     'lib.cogs.applymod',
     'lib.events.events_on_message',
     'lib.events.events_on_ready',
     'lib.events.events_cmd_error',
     'lib.events.events_on_join',
-    #'lib.cogs.tag',
     'lib.events.tasks_rainbow_role',
     'lib.events.tasks_channel',
     'lib.cogs.on_message',
